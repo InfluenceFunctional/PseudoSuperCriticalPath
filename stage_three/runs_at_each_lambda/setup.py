fc55eaab3b30017bbbcb9550c22b8c7a9891eee5
InfluenceFunctional <mjakilgour@gmail.com>
2024-11-26 11:18:52 -0500

    :> prep for stage 2

--- stage_three/runs_at_each_lambda/setup.py
+++ stage_three/runs_at_each_lambda/setup.py
@@ -11,27 +11,28 @@ def main():
     for i in range(1, number_steps // number_restart_steps + 1):
         current_restart_step = i * number_restart_steps
         current_lambda = current_restart_step / number_steps
+        current_lambda_str = str(current_lambda)
 
         try:
-            os.mkdir(f"/lambda_{current_lambda}")
+            os.mkdir(f"/lambda_{current_lambda_str}")
         except FileExistsError:
             pass
 
         copy(f"../generate_restart_files_lambda//stage_three.restart.{current_restart_step}",
-             f"/lambda_{current_lambda}/")
+             f"/lambda_{current_lambda_str}/")
 
         copy(f"../generate_restart_files_lambda//system.in.settings.hybrid_overlay",
-             f"/lambda_{current_lambda}/")
+             f"/lambda_{current_lambda_str}/")
 
         with (open(f"../generate_restart_files_lambda//create_atoms.txt", "r") as read,
-              open(f"/lambda_{current_lambda}/coeffs.txt", "w") as write):
+              open(f"/lambda_{current_lambda_str}/coeffs.txt", "w") as write):
             for line in read:
                 if line.startswith("mass") or line.startswith("create_atoms"):
                     continue
                 write.write(line)
 
         with (open("run_MD.lmp", "r") as read,
-              open(f"/lambda_{current_lambda}/run_MD.lmp", "w") as write):
+              open(f"/lambda_{current_lambda_str}/run_MD.lmp", "w") as write):
             text = read.read()
             text = text.replace("_T_SAMPLE", str(reference_temperature))
             text = text.replace("_N_STEPS", str(number_steps))
@@ -39,12 +40,12 @@ def main():
             text = text.replace("_RESTART_FILE", f"stage_three.restart.{current_restart_step}")
             write.write(text)
 
-        copy("sub_job.slurm", f"/lambda_{current_lambda}/sub_job.slurm")
+        copy("sub_job.slurm", f"/lambda_{current_lambda_str}/sub_job.slurm")
 
-        d = os.getcwd()
-        os.chdir(f"/lambda_{current_lambda}")
-        os.system("sbatch sub_job.slurm")
-        os.chdir(d)
+        # d = os.getcwd()
+        # os.chdir(f"/lambda_{current_lambda_str}")
+        # os.system("sbatch sub_job.slurm")
+        # os.chdir(d)
 
 
 if __name__ == '__main__':
