a45347e129edcd033ad3a314a00e8f7b9125ce78
InfluenceFunctional <mjakilgour@gmail.com>
2024-11-26 11:20:12 -0500

    :> prep for stage 2

--- stage_three/runs_at_each_lambda/setup.py
+++ stage_three/runs_at_each_lambda/setup.py
@@ -19,20 +19,20 @@ def main():
             pass
 
         copy(f"../generate_restart_files_lambda//stage_three.restart.{current_restart_step}",
-             f"/lambda_{current_lambda_str}/")
+             f"lambda_{current_lambda_str}/")
 
         copy(f"../generate_restart_files_lambda//system.in.settings.hybrid_overlay",
-             f"/lambda_{current_lambda_str}/")
+             f"lambda_{current_lambda_str}/")
 
         with (open(f"../generate_restart_files_lambda//create_atoms.txt", "r") as read,
-              open(f"/lambda_{current_lambda_str}/coeffs.txt", "w") as write):
+              open(f"lambda_{current_lambda_str}/coeffs.txt", "w") as write):
             for line in read:
                 if line.startswith("mass") or line.startswith("create_atoms"):
                     continue
                 write.write(line)
 
         with (open("run_MD.lmp", "r") as read,
-              open(f"/lambda_{current_lambda_str}/run_MD.lmp", "w") as write):
+              open(f"lambda_{current_lambda_str}/run_MD.lmp", "w") as write):
             text = read.read()
             text = text.replace("_T_SAMPLE", str(reference_temperature))
             text = text.replace("_N_STEPS", str(number_steps))
@@ -40,7 +40,7 @@ def main():
             text = text.replace("_RESTART_FILE", f"stage_three.restart.{current_restart_step}")
             write.write(text)
 
-        copy("sub_job.slurm", f"/lambda_{current_lambda_str}/sub_job.slurm")
+        copy("sub_job.slurm", f"lambda_{current_lambda_str}/sub_job.slurm")
 
         # d = os.getcwd()
         # os.chdir(f"/lambda_{current_lambda_str}")
