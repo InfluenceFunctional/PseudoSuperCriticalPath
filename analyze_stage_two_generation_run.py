2ab4030d9291f70c680c897117aa139335ee7f04
InfluenceFunctional <mjakilgour@gmail.com>
2025-01-21 19:33:48 -0500

    :> stage one restart runs

--- analyze_stage_two_generation_run.py
+++ analyze_stage_two_generation_run.py
@@ -49,10 +49,11 @@ def plot_run_thermo(lmbd):
 
 
 def analyze_gen_runs(show_figs=False):
-    head_directory = Path(r'D:\crystal_datasets\pscp\stage_one\generate_restart_files_lambda')
+    head_directory = Path(r'D:\crystal_datasets\pscp\stage_two\generate_restart_files_lambda')
     os.chdir(head_directory)
-    dirs = os.listdir()
-    dirs = [dir for dir in dirs if 'well_width' in dir]
+    #dirs = os.listdir()
+    #dirs = [dir for dir in dirs if 'well_width' in dir]
+    dirs = [os.getcwd()]
     en_records = []
     for dir in dirs:
         run_directory = head_directory.joinpath(Path(dir))
@@ -71,22 +72,23 @@ def analyze_gen_runs(show_figs=False):
     sigma = 100
     normed_ens = en_records / np.abs(np.mean(en_records, axis=1))[:, None]
     smoothed_ens = gaussian_filter1d(normed_ens, sigma=sigma)
-    diffs = np.diff(smoothed_ens,axis=1)
+    diffs = np.diff(smoothed_ens, axis=1)
     grads = np.diff(diffs, axis=1)
 
     fig = make_subplots(rows=1, cols=3, subplot_titles=['Energy', '1st deriv', '2nd deriv'])
     for ind in range(len(en_records)):
-        fig.add_scatter(y=en_records[ind]/np.abs(en_records[ind].mean()), row=1,col=1, name=dirs[ind], legendgroup=dirs[ind], showlegend=True)
-        fig.add_scatter(y=diffs[ind], row=1,col=2, name=dirs[ind], legendgroup=dirs[ind], showlegend=False)
-        fig.add_scatter(y=grads[ind], row=1,col=3, name=dirs[ind], legendgroup=dirs[ind], showlegend=False)
+        fig.add_scatter(y=en_records[ind] / np.abs(en_records[ind].mean()), row=1, col=1, name=dirs[ind],
+                        legendgroup=dirs[ind], showlegend=True)
+        fig.add_scatter(y=diffs[ind], row=1, col=2, name=dirs[ind], legendgroup=dirs[ind], showlegend=False)
+        fig.add_scatter(y=grads[ind], row=1, col=3, name=dirs[ind], legendgroup=dirs[ind], showlegend=False)
     fig.show(renderer='browser')
 
     well_width = [0.1, 0.5, 1.0, 2.0, 4.0]
-    well_depth = [0.01, 0.05, 0.1, 0.5, 1.0, 2.0, 4.0]
-    max_grad = np.max(np.abs(grads),axis=1)
+    well_depth = [0.1, 0.5, 1.0, 2.0, 4.0]
+    max_grad = np.max(np.abs(grads), axis=1)
 
     fig = go.Figure()
-    fig.add_heatmap(x=well_depth, y=well_width, z=max_grad.reshape(len(well_depth), len(well_width)).T)
+    fig.add_heatmap(x=well_depth, y=well_width, z=max_grad.reshape(len(well_width), len(well_depth)))
     fig.update_layout(xaxis_title='well_depth', yaxis_title='well_width')
     fig.show(renderer='browser')
 
@@ -105,15 +107,14 @@ def analyze_gen_runs(show_figs=False):
     try to predict local overlaps
     '''
 
-    energy = data['PotEng']
-
-    num_lambda_steps = 100
-    #lambda_steps = optimize_lambda_spacing3(np.array(energy), num_lambda_steps=num_lambda_steps, buffer=50, maxiter=200)
-    lambda_steps = optimize_lambda_spacing(np.array(data['Time'])[::10],
-                                           np.array(energy[::10]),
-                                           num_lambda_steps=num_lambda_steps,
-                                           buffer=10,
-                                           maxiter=1000)
+    # energy = data['PotEng']
+    #
+    # num_lambda_steps = 100
+    # #lambda_steps = optimize_lambda_spacing3(np.array(energy), num_lambda_steps=num_lambda_steps, buffer=50, maxiter=200)
+    # lambda_steps = optimize_lambda_spacing(np.array(energy[::10]),
+    #                                        num_lambda_steps=num_lambda_steps,
+    #                                        buffer=10,
+    #                                        maxiter=1000)
 
 
 def make_run_thermo_fig(data, dir, sigma=0.01):
@@ -131,24 +132,24 @@ def make_run_thermo_fig(data, dir, sigma=0.01):
     fig.show(renderer='browser')
 
 
-def spacing_analysis_fig(time, energy, lambda_steps, lambdas, buffer):
+def spacing_analysis_fig(data, energy, lambda_steps, lambdas, buffer):
     lambda_energies, lambda_widths, nn_overlaps, overlaps = (
         batch_compute_overlaps(buffer, energy, lambda_steps))
     fig = make_subplots(rows=1, cols=3,
                         subplot_titles=['Energy and lambda points', 'Overlaps', 'Nearest-neighbor overlaps'])
-    fig.add_scatter(x=time,
+    fig.add_scatter(x=data['Time'],
                     y=energy,
                     name='Energy',
                     row=1, col=1
                     )
-    fig.add_scatter(x=time[lambda_steps],
+    fig.add_scatter(x=data['Time'][lambda_steps],
                     y=lambda_energies,
                     error_y=dict(type='data', array=lambda_widths, visible=True),
                     showlegend=True,
                     name='Binned Energy',
                     row=1, col=1
                     )
-    fig.add_heatmap(z=overlaps,
+    fig.add_heatmap(z=np.log(overlaps),
                     row=1, col=2
                     )
     fig.add_scatter(x=lambdas[lambda_steps][1:],
@@ -160,7 +161,7 @@ def spacing_analysis_fig(time, energy, lambda_steps, lambdas, buffer):
     fig.show(renderer='browser')
 
 
-def optimize_lambda_spacing(time, energy, num_lambda_steps, buffer, maxiter=200, showfigs=True):
+def optimize_lambda_spacing(energy, num_lambda_steps, buffer, maxiter=200):
     all_steps = np.arange(0, len(energy))
     lambda_energies, lambda_widths, full_nn_overlaps, full_overlaps = batch_compute_overlaps(buffer, energy, all_steps)
 
@@ -169,40 +170,37 @@ def optimize_lambda_spacing(time, energy, num_lambda_steps, buffer, maxiter=200,
     lambda_energies, lambda_widths, nn_overlaps, overlaps = (
         batch_compute_overlaps(buffer, energy, current_steps))
 
+    iter = 0
+    tolerance = 1
     converged = False
     overlaps_record = []
     states_record = []
-    target_overlap = 0.1
-    ii = -1
-    while not converged:
-        ii += 1
-        print(ii)
+    while not converged and iter < maxiter:
+        if nn_overlaps.min() < nn_overlaps.mean() * 0.5:
+            tolerance *= 0.975
+        target_overlap = nn_overlaps.mean() * tolerance
+
         greedy_steps = []
         i0 = 0
         greedy_steps.append(i0)
         for s_ind in range(num_lambda_steps - 1):
             state_overlaps = full_overlaps[i0]
             valid_options = state_overlaps >= target_overlap
-            if np.sum(valid_options) == 0:
-                converged = True
-                break
-            continuity_block = np.argwhere(~valid_options)
-            block_state = continuity_block[continuity_block > i0]
-            if len(block_state) > 0:
-                block_state = block_state[0]
-            else:
-                block_state = len(energy) - 1
-            pick_state = np.amax(np.where(valid_options[:block_state]))  # pick the furthest contiguous valid state
-            if pick_state <= i0:
-                #assert False, "No valid overlaps after this given state"
-                print("overlap algo too greedy")
-                break
-
+            pick_state = np.amax(np.where(valid_options))  # pick the furthest valid state
+            assert pick_state > i0, "No valid overlaps after this given state"
             greedy_steps.append(pick_state)
-
             i0 = pick_state
             if i0 == len(energy) - 1:
-                converged = True
+                #print("Ran out of states!")
+                tolerance *= 1.01
+
+                # fill in with randoms
+                while ((num_lambda_steps) - len(greedy_steps)) > 0:
+                    randint = np.random.randint(len(energy) - 2)
+                    if randint not in greedy_steps:
+                        greedy_steps.append(randint)
+                        greedy_steps.sort()
+
                 break
 
         lambda_energies, lambda_widths, nn_overlaps, overlaps = (
@@ -213,63 +211,45 @@ def optimize_lambda_spacing(time, energy, num_lambda_steps, buffer, maxiter=200,
         greedy_steps[-1] = len(energy) - 1
         states_record.append(greedy_steps)
         current_steps = np.array(greedy_steps)
-        target_overlap *= 1.01
 
-    min_record = np.array([ov.min() if len(ov) > 0 else 0 for ov in overlaps_record])
-    mean_record = np.array([ov.mean() if len(ov) > 0 else 0 for ov in overlaps_record])
-    if showfigs:
-        import plotly.graph_objects as go
-        fig = go.Figure()
-        fig.add_scatter(y=min_record, name='min')
-        fig.add_scatter(y=mean_record, name='mean')
-        fig.show(renderer='browser')
-        go.Figure(go.Scatter(y=min_record * mean_record, name='combo score')).show(renderer='browser')
+        iter += 1
 
-    best_state_ind = np.argmax(min_record) - 1
-    best_state = np.asarray(states_record[best_state_ind])
-    # mins = []
-    # exp_states = []
-    # opt_exponents = np.concatenate([np.linspace(0.1, 1.2, 20), np.ones(1)])
-    # for exp in opt_exponents:
-    #     exp_state = np.copy(best_greedy_state)
-    #     exp_state = ((exp_state / len(energy)) ** exp) * len(energy)  # rescale slightly
-    #     exp_state = exp_state.astype(int)
-    #     lambda_energies, lambda_widths, nn_overlaps, overlaps = (
-    #         batch_compute_overlaps(buffer, energy, exp_state))
-    #     mins.append(nn_overlaps.min())
-    #     exp_states.append(exp_state)
-    #
-    # best_state = exp_states[np.argmax(np.array(mins))]
-    if showfigs:
-        lambdas = np.linspace(0, 1, len(energy))
-        spacing_analysis_fig(time, energy, best_state, lambdas, buffer=buffer)
+    min_record = np.array([ov.min() for ov in overlaps_record])
+    mean_record = np.array([ov.mean() for ov in overlaps_record])
+    import plotly.graph_objects as go
+    fig = go.Figure()
+    fig.add_scatter(y=min_record, name='min')
+    fig.add_scatter(y=mean_record, name='mean')
+    fig.show(renderer='browser')
+    go.Figure(go.Scatter(y=min_record * mean_record, name='combo score')).show(renderer='browser')
 
-        fig = go.Figure(
-            go.Scatter(x=np.linspace(0, 1, len(best_state)), y=best_state / best_state[-1], mode='lines+markers'))
-        fig.add_scatter(x=np.linspace(0, 1, len(best_state)), y=np.linspace(0, 1, len(best_state))).show(renderer='browser')
+    best_greedy_state = np.asarray(states_record[np.argmax(min_record)])
+    mins = []
+    exp_states = []
+    opt_exponents = np.concatenate([np.linspace(0.1, 1.2, 20), np.ones(1)])
+    for exp in opt_exponents:
+        exp_state = np.copy(best_greedy_state)
+        exp_state = ((exp_state / len(energy)) ** exp) * len(energy)  # rescale slightly
+        exp_state = exp_state.astype(int)
+        lambda_energies, lambda_widths, nn_overlaps, overlaps = (
+            batch_compute_overlaps(buffer, energy, exp_state))
+        mins.append(nn_overlaps.min())
+        exp_states.append(exp_state)
 
-    print(f"Mean nn overlap is {mean_record[best_state_ind]:.2f}")
-    print(f"Min nn overlap is {min_record[best_state_ind]:.2f}")
-    return best_state
+    best_state = exp_states[np.argmax(np.array(mins))]
 
-
-def extract_lambda_spacing(num_lambda_steps, num_restart_paths, gen_restart_path: str, buffer=10):
-    run_directory = Path(gen_restart_path)
+    run_directory = Path(r'D:\crystal_datasets\pscp\stage_three\generate_restart_files_lambda')
     log_file = run_directory.joinpath("screen.log")
     data = pd.read_csv(log_file, skiprows=compose_row_function(log_file, False), sep=r"\s+")
-    energy = data['PotEng']
 
-    subsample_ratio = len(energy) // num_restart_paths
+    lambdas = np.linspace(0, 1, len(energy))
+    spacing_analysis_fig(data, energy, best_state, lambdas, buffer=buffer)
 
-    lambda_steps = optimize_lambda_spacing(np.array(data['Time'])[::subsample_ratio],
-                                           np.array(energy)[::subsample_ratio],
-                                           num_lambda_steps=num_lambda_steps,
-                                           buffer=buffer,
-                                           maxiter=1000,
-                                           showfigs=False)
+    fig = go.Figure(
+        go.Scatter(x=np.linspace(0, 1, len(best_state)), y=best_state / best_state[-1], mode='lines+markers'))
+    fig.add_scatter(x=np.linspace(0, 1, len(best_state)), y=np.linspace(0, 1, len(best_state))).show(renderer='browser')
 
-    return lambda_steps
-
+    return best_state
 
 
 def batch_compute_overlaps(buffer, energy, steps_to_sample):
--- stage_one/runs_at_each_lambda/setup.py
+++ stage_one/runs_at_each_lambda/setup.py
@@ -1,58 +1,76 @@
 import os
 from shutil import copy
 
+import numpy as np
+from pathlib import Path
+from analyze_stage_one_generation_run import extract_lambda_spacing
+
 
 def main():
     reference_temperature = 400
-    # 4.0 estimated from Mathematica script, 0.9 used by them, 0.09 reported in the paper.
-    well_widths_angstrom_squared = [4.0, 0.9, 0.09]
 
     number_steps = 2000000
-    number_restart_steps = 20000  # 100 restart files
+    number_restart_steps = 2000  # 100 restart files
+    dir_name = 'well_width_0.05_well_depth_0.1'
+    gen_path = Path('../generate_restart_files_lambda').joinpath(Path(dir_name))
+    restart_paths = os.listdir(gen_path)
+    restart_paths = [p for p in restart_paths if 'stage_one.restart' in p]
 
-    for well_width in well_widths_angstrom_squared:
+    steps_to_restart = extract_lambda_spacing(100,
+                                              len(restart_paths),
+                                              gen_path,
+                                              buffer=10,
+                                              )
+
+    steps_to_restart[0] = 1
+    steps_to_restart[-1] = 999
+    print(f"running at lambda points {steps_to_restart}")
+    restart_steps = np.linspace(number_steps // 1000, number_steps, 1000).astype(int)
+
+    try:
+        os.mkdir(dir_name)
+    except FileExistsError:
+        pass
+
+    for i in range(len(steps_to_restart)):
+        current_restart_step = steps_to_restart[i]
+        current_lambda = float(current_restart_step / 1000)
+        restart_step = restart_steps[current_restart_step]
+        restart_step_string = str(restart_step)
+
         try:
-            os.mkdir(f"well_width_{well_width}")
+            os.mkdir(f"{dir_name}/lambda_{restart_step_string}")
         except FileExistsError:
             pass
 
-        for i in range(1, number_steps // number_restart_steps + 1):
-            current_restart_step = i * number_restart_steps
-            current_lambda = current_restart_step / number_steps
+        copy(f"../generate_restart_files_lambda/{dir_name}/stage_one.restart.{restart_step_string}",
+             f"{dir_name}/lambda_{restart_step_string}/")
 
-            try:
-                os.mkdir(f"well_width_{well_width}/lambda_{current_lambda}")
-            except FileExistsError:
-                pass
+        copy(f"../generate_restart_files_lambda/{dir_name}/system.in.settings.hybrid_overlay",
+             f"{dir_name}/lambda_{restart_step_string}/")
 
-            copy(f"../generate_restart_files_lambda/well_width_{well_width}/stage_one.restart.{current_restart_step}",
-                 f"well_width_{well_width}/lambda_{current_lambda}/")
+        with (open(f"../generate_restart_files_lambda/{dir_name}/create_atoms.txt", "r") as read,
+              open(f"{dir_name}/lambda_{restart_step_string}/coeffs.txt", "w") as write):
+            for line in read:
+                if line.startswith("mass") or line.startswith("create_atoms"):
+                    continue
+                write.write(line)
 
-            copy(f"../generate_restart_files_lambda/well_width_{well_width}/system.in.settings.hybrid_overlay",
-                 f"well_width_{well_width}/lambda_{current_lambda}/")
+        with (open("run_MD.lmp", "r") as read,
+              open(f"{dir_name}/lambda_{restart_step_string}/run_MD.lmp", "w") as write):
+            text = read.read()
+            text = text.replace("_T_SAMPLE", str(reference_temperature))
+            text = text.replace("_N_STEPS", str(number_steps))
+            text = text.replace("_LAMBDA", str(current_lambda))
+            text = text.replace("_RESTART_FILE", f"stage_one.restart.{restart_step_string}")
+            write.write(text)
 
-            with (open(f"../generate_restart_files_lambda/well_width_{well_width}/create_atoms.txt", "r") as read,
-                  open(f"well_width_{well_width}/lambda_{current_lambda}/coeffs.txt", "w") as write):
-                for line in read:
-                    if line.startswith("mass") or line.startswith("create_atoms"):
-                        continue
-                    write.write(line)
+        copy("sub_job.slurm", f"{dir_name}/lambda_{restart_step_string}/sub_job.slurm")
 
-            with (open("run_MD.lmp", "r") as read,
-                  open(f"well_width_{well_width}/lambda_{current_lambda}/run_MD.lmp", "w") as write):
-                text = read.read()
-                text = text.replace("_T_SAMPLE", str(reference_temperature))
-                text = text.replace("_N_STEPS", str(number_steps))
-                text = text.replace("_LAMBDA", str(current_lambda))
-                text = text.replace("_RESTART_FILE", f"stage_one.restart.{current_restart_step}")
-                write.write(text)
-
-            copy("sub_job.slurm", f"well_width_{well_width}/lambda_{current_lambda}/sub_job.slurm")
-
-            d = os.getcwd()
-            os.chdir(f"well_width_{well_width}/lambda_{current_lambda}")
-            os.system("sbatch sub_job.slurm")
-            os.chdir(d)
+        d = os.getcwd()
+        os.chdir(f"{dir_name}/lambda_{restart_step_string}")
+        os.system("sbatch sub_job.slurm")
+        os.chdir(d)
 
 
 if __name__ == '__main__':
